14f0f68223b634a7010e44b603f1c331b3b28bf7
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-07-24 21:12:09 -0600

    Fix: Correction in the height of the  sistem logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body style="margin: 0; padding: 0; box-sizing: border-box; font-size: 62.5%;">
         @inertia
     </body>
 </html>
