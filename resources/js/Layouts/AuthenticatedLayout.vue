4f89b97b37233c0675c2f3e1c0684792b3018544
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-08-04 23:27:49 -0600

    Feat: Add the responsive for mobiles devices

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,7 +4,6 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
@@ -25,7 +24,7 @@ const toggleSidebar = () => {
     <!-- Primary Navigation Menu -->
     <div class="container-page" ref="containerPage">
         <aside class="sidebar" ref="sidebarCollapse">
-            <div class="items-sidebar ">
+            <div class="items-sidebar">
                 <!-- Logo -->
                 <div class="logo-sidebar">
                     <Link :href="route('dashboard')">
@@ -85,10 +84,13 @@ const toggleSidebar = () => {
                         <span class="dropdown-content">
                             <DropdownLink :href="route('profile.edit')">
                                 <img src="/icons/profile-icon.gif" alt="Perfil" title="Perfil" class="img-logout" />
-                                <span class="span-logout">Perfil</span></DropdownLink>
+                                <span class="span-logout">Perfil</span>
+                            </DropdownLink>
                             <DropdownLink :href="route('logout')" method="post" as="button">
-                            <img src="/icons/logout-icon.gif" alt="Cerrar sesión" title="Cerrar sesión" class="img-logout">
-                            <span class="span-logout">Cerrar sesión</span></DropdownLink>
+                                <img src="/icons/logout-icon.gif" alt="Cerrar sesión" title="Cerrar sesión"
+                                    class="img-logout">
+                                <span class="span-logout">Cerrar sesión</span>
+                            </DropdownLink>
                         </span>
                     </template>
 
@@ -104,25 +106,42 @@ const toggleSidebar = () => {
 
                 </Dropdown>
             </div>
-            <!-- Hamburger -->
-            <!--
-                Esto es para
-               -->
-            <!-- <div>
-                <button @click="showingNavigationDropdown = !showingNavigationDropdown">
-                    <svg stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{ hidden: showingNavigationDropdown, 'inline-flex': !showingNavigationDropdown }"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{ hidden: !showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div> -->
         </aside>
 
         <!-- Page Heading -->
+        <div class="container-menu-mobile">
+            <div class="menu-mobile">
+                <img src="/icons/hamburguer.png" alt="Menu" class="hamburguer-icon" @click="toggleSidebar" />
+            </div>
 
+            <div class="nav-links-mobile" ref="sidebarCollapse">
+                        <NavLink :href="route('units.index')" :active="route().current('units.index')">
+                            <img src="/icons/themes-icon.gif" alt="Temas de estudio" class="icons-sidebar" />
+                            <span class="hidden" style="--i:1">Temas de estudio</span>
+                        </NavLink>
+                        <NavLink :href="route('levels.index')" :active="route().current('levels.index')">
+                            <img src="/icons/levels-icon.gif" alt="Niveles" class="icons-sidebar" />
+                            <span class="hidden" style="--i:2">Niveles</span>
+                        </NavLink>
+                        <NavLink :href="route('lessons.index')" :active="route().current('lessons.index')">
+                            <img src="/icons/lections-icon.gif" alt="Lecciones" class="icons-sidebar" />
+                            <span class="hidden" style="--i:3">Lecciones</span>
+                        </NavLink>
+                        <NavLink :href="route('resources.index')" :active="route().current('resources.index')">
+                            <img src="/icons/resources-icon.gif" alt="Recursos" class="icons-sidebar" />
+                            <span class="hidden" style="--i:4">Recursos</span>
+                        </NavLink>
+                        <NavLink :href="route('exercises.index')" :active="route().current('exercises.index')">
+                            <img src="/icons/challenge-icon.gif" alt="Ejercicios" class="icons-sidebar" />
+                            <span class="hidden" style="--i:5">Ejercicios</span>
+                        </NavLink>
+                        <NavLink :href="route('admin.progress.index')"
+                            :active="route().current('admin.progress.index')">
+                            <img src="/icons/progress-icon.gif" alt="" class="icons-sidebar" />
+                            <span class="hidden" style="--i:6">Progreso estudiantes</span>
+                        </NavLink>
+            </div>
+        </div>
 
         <!-- Page Content -->
         <main>
