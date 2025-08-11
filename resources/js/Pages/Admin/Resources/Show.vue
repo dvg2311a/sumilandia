b9d539a1b8917abedb6205b553c1aa01f021a85b
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-08-10 21:43:30 -0600

    feat(show):add new styles to view show in the component resources

--- resources/js/Pages/Admin/Resources/Show.vue
+++ resources/js/Pages/Admin/Resources/Show.vue
@@ -11,34 +11,24 @@ const props = defineProps({
 
     <Head title="Detalle de Recurso" />
     <AuthenticatedLayout>
-        <template #header>
-            <div class="flex items-center justify-between">
-                <h2 class="text-xl font-semibold leading-tight text-gray-800">Detalle de Recurso</h2>
+        <div class="page-content">
+            <div class="back">
+                <Link href="/resources">
+                <img src="/icons/return-icon.gif" alt="Regresar a la página anterior" class="btn-back" />
+                </Link>
             </div>
-        </template>
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6">
-                        <div class="flex justify-between items-center mb-6">
-                            <h1 class="text-2xl font-bold text-gray-800">{{ resource.name }}</h1>
-                            <Link href="/resources"
-                                class="bg-gray-200 text-gray-700 px-4 py-2 rounded hover:bg-gray-300">Volver
-                            </Link>
-                        </div>
-                        <div class="mb-4">
-                            <strong>Descripción:</strong> {{ resource.description }}
-                        </div>
-                        <div class="mb-4">
-                            <strong>Archivo:</strong> <a :href="`/storage/${props.resource.file_path}`" target="_blank"
-                                class="text-blue-600 underline">Ver archivo</a>
-                        </div>
-                        <div class="mb-4">
-                            <strong>Unidad:</strong> {{ resource.unit?.name }}
-                        </div>
-                    </div>
+            <div class="card">
+                <div class="header-actions">
+                    <h1>Detalles del recurso</h1>
+                </div>
+                <div class="unit-details">
+                    <p><strong>ID</strong> {{ resource.id }}</p>
+                    <p><strong>Nombre</strong> {{ resource.name }}</p>
+                    <p><strong>Descripción:</strong> {{ resource.description }}</p>
+                    <p><strong>Unidad:</strong> {{ resource.unit?.name }}</p>
                 </div>
             </div>
         </div>
+
     </AuthenticatedLayout>
 </template>
